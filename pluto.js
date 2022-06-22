f17962dcff462f7ee9bb82a978d1d022be71f25e
vanslembrouck <justinvan@gmail.com>
2022-06-21 20:34:42 -0400

    renaming pluto repository to pluto-app

--- pluto.js
+++ pluto.js
@@ -1,8 +1,18 @@
+//MIDI CC IN
+const SET_VOICE1_MIDI_IN  = 85;
+const SET_VOICE1_MIDI_OUT = 86;
+const SET_VOICE2_MIDI_IN  = 87;
+const SET_VOICE2_MIDI_OUT = 88;
+const SAVE_MIDI_SETTINGS  = 89;
 
-
+var tempoutput;
 
 let midiOutput = null;
+let plutoOutput = null;
 let currentSequenceId = -1;
+
+var midiChannel = 1;
+var plutoId;
 
 const START = 41;
 
@@ -13,6 +23,11 @@ const NOTE_ON = 0x90;     //144
 const NOTE_OFF = 0x80;    //128
 const CC = 0xB0;          //176
 const NOTE_DURATION = 200;
+
+const NOTE_ON_DECIMAL = 144;     //144
+const NOTE_OFF_DECIMAL = 128;    //128
+const CC_DECIMAL = 176;          //176
+
 
 
 let searchingMessages = [
@@ -26,14 +41,14 @@ searchingMessageIndex = 0;
 
 const playNote = function() {
   if (currentSequenceId >= 0) {
-    midiOutput.send([NOTE_OFF, sequence[currentSequenceId], 0x7f]); //0x7f = 127
+    plutoOutput.send([NOTE_OFF, sequence[currentSequenceId], 0x7f]); //0x7f = 127
   }
 
   currentSequenceId++;
   if (currentSequenceId >= sequence.length) {
     currentSequenceId = 0;
   }
-  midiOutput.send([NOTE_ON, sequence[currentSequenceId], 0x7f]);
+  plutoOutput.send([NOTE_ON, sequence[currentSequenceId], 0x7f]);
 
   setTimeout(playNote, NOTE_DURATION);
 }
@@ -41,7 +56,7 @@ const playNote = function() {
 
 
 const sendMessage = function(){
-  midiOutput.send([CC, 115, 0x7f]);
+  plutoOutput.send([CC, 115, 0x7f]);
 
   dump = [0];
   testArray = [255, 4, 5, 6, 7, 3, 4, 5, 6, 7, 3, 4, 5, 6, 7, 3, 4, 5, 6, 99,
@@ -56,30 +71,29 @@ const sendMessage = function(){
   dump[0] = 0xF0;
   dump[testArray.length+1] = 0xF7;
 
-  midiOutput.send(dump); 
+  plutoOutput.send(dump); 
 
 }
 
 
-function sendSimpleCC(control, value) {
-  var message = [CC, control, value];    
-  midiOutput.send(message);
-  
+function sendSimpleCC(channel, control, value) {
+  // var message = [CC, control, value];    
+  var message = [CC_DECIMAL + channel - 1, control, value];
+  plutoOutput.send(message);
 }
 
 
 
 
 if (!('requestMIDIAccess' in navigator)) {
-  alert('Unable to access MIDI devices. Make sure your devices are connected and try using the Chrome broswer.');
+  //alert('Unable to access MIDI devices. Make sure your devices are connected and try using the Chrome broswer.');
   // console.log('Could not access your MIDI devices.'); 
 
 } else {
 
   navigator.requestMIDIAccess( { sysex: true } ).then( onMIDISuccess, onMIDIFailure );
 
-  //navigator.requestMIDIAccess()
-   //   .then(onMIDISuccess, onMIDIFailure);
+
 }
 
 
@@ -130,14 +144,16 @@ function checkForPluto(access) {
           
       midiInput.onmidimessage = function(message) {  
 
-
+      if (message.currentTarget.id = plutoId){
+        //console.log('receiving message from Pluto', message);
+      }
       
       //ignore clock messages
       if (message.data[0] != 248){
         data = message.data; // this gives us our [command/channel, note, velocity] data.
         console.log('MIDI data', data); // MIDI data [144, 63, 73]
 
-        if (data[0] == 240){
+        if (data[0] == 0xF0){
           console.log("receiveSysex");
           receiveSysex(data);
         }
@@ -161,6 +177,12 @@ function checkForPluto(access) {
       if (midiOutput.name == "Pluto" && midiOutput.manufacturer == "Modern Sounds"){
          //outputText.push(`✓ Pluto MIDI output detected`);
           plutoDetected += 1;
+          plutoId = midiOutput.id;
+
+          plutoOutput = midiOutput; //set this so we can target only Pluto when multiple devices are present
+        
+
+          console.log("Pluto port id:" + plutoId);
       }
     })
       
@@ -184,7 +206,9 @@ function checkForPluto(access) {
       outputText.push(`Pluto connected!`);
 
       //sendMessage();
-      sendSimpleCC(77, 77);
+      
+      doHandshake();
+      //sendSimpleCC(1, 77, 77);
     }
 
     document.querySelector("#inputs").innerText = inputText.join('');
@@ -194,6 +218,35 @@ function checkForPluto(access) {
  
 }
 
+var channelToTry = 0;
+let checkInterval;
+
+function doHandshake(){
+
+
+
+  checkInterval = setInterval(checkMIDI, 40);
+
+  function checkMIDI()
+  {
+   
+   channelToTry++;
+
+   if (channelToTry >= 16){
+    clearInterval(checkInterval);
+   } else {
+    console.log(channelToTry);
+
+    sendSimpleCC(channelToTry, 77, 77);
+   }
+
+  }
+
+
+
+}
+
+
 function onMIDIFailure() {
     console.log('Could not access your MIDI devices.');
 }
@@ -201,14 +254,31 @@ function onMIDIFailure() {
 
 
 function receiveSysex(data){
+  clearInterval(checkInterval);
+  
+  console.log(data);
+
 
   var version = data[1];
 
-  for (i=2; i < data.length - 1; i++){
+  for (i=2; i < 4; i++){
       version += '.' + data[i];
   }
 
+  let in1 = data[4];
+  let out1 = data[5];
+  let out2 = data[6];
+
+  midiChannel = in1;
+
   document.querySelector("#messages").innerText += "Firmware " + version;
+  document.querySelector("#midiInput").innerText += "MIDI input channel " + in1;
+  document.querySelector("#midiOutput").innerText += "MIDI output channels " + out1 + ", " + out2;
+
+  document.getElementById("midiInDropdown").selectedIndex = 7; //Option 10
+  document.getElementById("midiOutVoice1Dropdown").selectedIndex = 8; //Option 10
+  document.getElementById("midiOutVoice2Dropdown").selectedIndex = 9; //Option 10
+
 }
 
 
@@ -220,8 +290,35 @@ function showVal(newVal, span){
 
 function updateSlider(control, value, label){
     document.getElementById(label).innerHTML=value;
-    sendSimpleCC(control, value); 
+    sendSimpleCC(midiChannel, control, value); 
     console.log(value);
+}
+
+function GetSelectedItem(el)
+{
+
+    var e = document.getElementById(el);
+    var value = e.options[e.selectedIndex].value;
+    var strSel = "The Value is: " + value + " and text is: " + e.options[e.selectedIndex].text;
+
+    console.log(strSel);
+    sendSimpleCC(midiChannel, SET_VOICE1_MIDI_IN, value); 
+}
+
+
+function clickButton(control, value){
+
+  var e = document.getElementById(value);
+  var s = e.options[e.selectedIndex].text;
+  console.log(s);
+
+    sendSimpleCC(midiChannel, control, s); 
+    console.log(value);
+}
+
+
+function saveMidiSettings(){
+  sendSimpleCC(midiChannel, SAVE_MIDI_SETTINGS, 0); 
 }
 
 const mySlider = document.getElementById('slider1');
